9e02760a77d58a97617745a584ccba4398258354
scar1997 <francy-scardy@hotmail.it>
2018-07-15 00:03:34 +0200

    add overlay

---
+++ src/js/overlay.js
@@ -0,0 +1,10 @@
+
+/* Open */
+function open_overlay() {
+    document.getElementById("overlay_id").style.height = "100%";
+}
+
+/* Close */
+function close_overlay() {
+    document.getElementById("overlay_id").style.height = "0%";
+}
